cb531b637aa4dac861e314ecf141b9e36b1ca9be
barbie1kent <bobrenev.daniil2006@gmail.com>
2023-12-17 22:23:22 +0300

    Добавьте файлы проекта.

---
+++ mlstart_caesarchipher/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class CaesarCipher
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите текст для шифрования: ");
+        string input = Console.ReadLine();
+
+        Console.WriteLine("Введите сдвиг: ");
+        int shift = Convert.ToInt32(Console.ReadLine());
+
+        string encryptedText = Encrypt(input, shift);
+        Console.WriteLine("Зашифрованный текст: " + encryptedText);
+    }
+
+    static string Encrypt(string input, int shift)
+    {
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        string encryptedText = "";
+
+        foreach (char c in input)
+        {
+            if (alphabet.Contains(c))
+            {
+                if (alphabet.IndexOf(c) + shift > 0)
+                {
+                    encryptedText += alphabet[(alphabet.IndexOf(c) + shift) % 26];
+                }
+                else
+                {
+                    encryptedText += alphabet[(alphabet.IndexOf(c) + shift + 26) % 26];
+                }
+            }
+            else
+            {
+                encryptedText += ' ';
+            }
+        }
+
+        return encryptedText;
+    }
+}
